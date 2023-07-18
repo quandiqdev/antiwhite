50e85bf3e7baccde011fe91aa377874d1ee674f9
quandiqdev <132442061+quandiqdev@users.noreply.github.com>
2023-07-18 19:08:36 +0500

    Add files via upload

--- src/components/header/header-main.jsx
+++ src/components/header/header-main.jsx
@@ -2,13 +2,14 @@ import mainLogo from "../../assets/images/main/header-main-logo.png"
 export default function HeaderMain() {
     return (
         <div className="header__main">
-            <div className="header__main-logo">
+
+            <div className="header__main-logo anim logo__anim">
                 <img src={mainLogo} alt="" />
             </div>
-            <div className="header__main-title">
+            <div className="header__main-title anim title__anim">
                 Invariance Records
             </div>
-            <div className="header__main-subtitle">
+            <div className="header__main-subtitle anim subtitle__anim">
                 Мы российский лейбл, занимающийся развитием музыкальной индустрии в СНГ
             </div>
         </div>
